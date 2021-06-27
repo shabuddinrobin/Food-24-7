e8e3a5efb5dc89109c9b4c55ebca2b6e0b0edc5a
shabuddinrobin <81437024+shabuddinrobin@users.noreply.github.com>
2021-06-27 20:37:12 +0600

    Update sign_up.js

--- sign_up.js
+++ sign_up.js
@@ -1,10 +1,6 @@
 var form = document.getElementById("form");
-var username = document.getElementById("username");
 var email = document.getElementById("email");
-var phone = document.getElementById("phone");
-var address = document.getElementById("address");
 var password = document.getElementById("pass1");
-var password2 = document.getElementById("pass2");
 
 // --------------------CLOCK ---------------------
 
@@ -31,56 +27,29 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 }
 
-function Validate(input, message) {
-    const formControl = input.parentElement;
-    formControl.className ='form-control error';
-    const small = formControl.querySelector('#small2');
-    small.innerText = message;     
-        }
 
 form.addEventListener('submit', function(e){
         e.preventDefault();
-        let pass1Val = document.getElementById("pass1").value;
-        let pass2Val = document.getElementById("pass2").value;
-    
             if(password.value === ''){
                 showError(password, "Password is required");
+            }
+            else if(password.value.length < 8){
+                showError(password, "Need 8 characters")
+            }
+            else if(password.value.length < 1){
+                showError(password, "Need 8 characters")
+            }
+            else if(password.value.length >= 9){
+                showError(password, "Only 8 characters indeed")
             }
                 else{
                     showSuccess(password);
                 }
-            if(password2.value === ''){
-                showError(password2, "Password retype is required");
-            }
-                else{
-                    showSuccess(password2);
-                }
-            if (pass1Val != pass2Val) {        
-                    Validate(password2, "Password Mismatched");
-                    }
-            if(username.value === ''){
-                showError(username, "Username is required");
-        }
-                else{
-                    showSuccess(username);
-            }
             if(email.value === ''){
                 showError(email, "email is required");
         }
                 else{
                     showSuccess(email);
-            }
-            if(phone.value === ''){
-                showError(phone, "Phone is required");
-        }
-                else{
-                    showSuccess(phone);
-            }
-            if(address.value === ''){
-                showError(address, "Address is required");
-        }
-                else{
-                    showSuccess(address);
             }
         }
 )
